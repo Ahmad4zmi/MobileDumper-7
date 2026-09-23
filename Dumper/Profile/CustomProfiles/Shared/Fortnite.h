92eec1c3dc35a470afe1fd46ff6180166e10865b
Mohammad Ahmad <mohammadahmadazmi7@gmail.com>
2026-09-23 12:47:49 +0530

    Create Fortnite.h

---
+++ Dumper/Profile/CustomProfiles/Shared/Fortnite.h
@@ -0,0 +1,12 @@
+void OverrideDecryptCallbacks(FDecryptCallbacks& Callbacks) const override
+{
+	Callbacks.ChunkedObjects.Objects = [](uintptr_t Value, uintptr_t /* Optional Address */)
+	{
+		return (0xDC1F96F8C0C17AA5LL * Value - 0x70D8A3D87E4765FCLL);
+	};
+
+	Callbacks.ChunkedObjects.NumElements = [](int32 Value, uintptr_t /* Optional Address */)
+	{
+		return -1225621317 * Value - 106502534;
+	};
+}
